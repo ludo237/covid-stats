40949498f8153457abdf76015a6f1399c14b9c1a
Claudio Ludovico <ludovico1990@hotmail.it>
2020-03-14 16:24:33 +0100

    Fix the way we build the panels

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="h-64 max-w-xl relative flex justify-center bg-white rounded px-5 pt-5 capitalize cursor-pointer shadow-sm transition duration-500 ease-in-out hover:shadow-2xl transform hover:-translate-y-1 hover:scale-120"
+    class="relative flex justify-center bg-white rounded px-5 pt-5 capitalize cursor-pointer shadow-sm transition duration-500 ease-in-out hover:shadow-2xl transform hover:-translate-y-1 hover:scale-120"
   >
     <div class="flex flex-col items-center ">
       <PanelIcon :icon="icon" :color="color" class="mb-2" />
--- src/components/PanelGrid.vue
+++ src/components/PanelGrid.vue
@@ -1,28 +1,33 @@
 <template>
-  <div
-    class="grid gap-5 md:gap-12 grid-flow-col md:grid-flow-row grid-cols-1"
-    :class="responsiveGrid"
-  >
-    <slot />
-  </div>
+	<div
+		class="grid gap-5 md:gap-12 grid-flow-col md:grid-flow-row"
+		:class="responsiveGrid"
+	>
+		<slot />
+	</div>
 </template>
 
 <script>
 export default {
-  name: "PanelGrid",
+	name: "PanelGrid",
 
-  props: {
-    cols: {
-      retuired: true,
-      type: [Number, String],
-      default: 1,
-    }
-  },
+	props: {
+		cols: {
+			retuired: true,
+			type: [Number, String],
+			default: 1
+		}
+	},
 
-  computed: {
-    responsiveGrid() {
-      return [`grid-rows-${this.cols}`, `md:grid-cols-${this.cols}`];
-    }
-  }
-}
+	computed: {
+		responsiveGrid() {
+			return [
+				`grid-rows-${this.cols}`,
+				"grid-cols-1",
+				`md:grid-cols-${this.cols}`,
+				"md:grid-rows-1"
+			];
+		}
+	}
+};
 </script>
