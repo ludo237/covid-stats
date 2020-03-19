c06dc7e7d6d8dbed5ffdc7eb972bc8e0ab57cc13
ludo237 <921500+ludo237@users.noreply.github.com>
2020-03-19 11:35:47 +0000

    Deploying to gh-pages from  - a02fc35b6eb63879cd975c39330c87363da3a481 🚀

---
+++ precache-manifest.81ae6a6b40045fc7dc35cf74499b4367.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800dbca2dce1cf7aaa2e",
+    "url": "/css/app.829a8b29.css"
+  },
+  {
+    "revision": "a94697830f1fa06ea9c6eee6a2686098",
+    "url": "/index.html"
+  },
+  {
+    "revision": "800dbca2dce1cf7aaa2e",
+    "url": "/js/app.3d0efb64.js"
+  },
+  {
+    "revision": "57ef85d35a327e62403e",
+    "url": "/js/chunk-vendors.66c36d64.js"
+  },
+  {
+    "revision": "97780ca5646134083074",
+    "url": "/js/daily-summary-chart.3fc4d870.js"
+  },
+  {
+    "revision": "63674f75c5d54fcf6fc9",
+    "url": "/js/date-picker.5dad55fe.js"
+  },
+  {
+    "revision": "689e56cacbc62e6857902c66dcc02e00",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
